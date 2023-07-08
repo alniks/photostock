6fb055b81b80cc3453b4febb39024174462f790d
Dasha <dsaluk@gmail.com>
2023-07-08 09:44:20 +0300

    #1 Data entry UI Part

--- photostock/src/main/js/App.js
+++ photostock/src/main/js/App.js
@@ -1,16 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import ReactDOM from "react-dom";
+import InputForm from "./components/InputForm";
 
 
-export class App extends Component {
-    render() {
+//export class App extends Component {
+    const App = () =>
+    //render()
+    {
+        const [values, setValues] = useState({
+            date: ""
+        });
+        const inputs = [
+            {
+                id:1,
+                name:"date",
+                type:"date",
+                placeholder:"Date",
+                label:"Date"
+                
+            }
+        ];
+        //const dataRef = useRef();
+        const handleAdd = (e) =>{
+            e.preventDefault();
+            
+        };
+        const onChange = (e) => {
+            setValues({...values, [e.target.name]:e.target.value});
+        };
         return (
-            <div>
-            <h1>Welcome to React Front End Served by Spring Boot</h1>
+                
+            <div className="app">
+            
+            <form onSubmit={handleAdd}>
+                    <h1>Welcome to React</h1>
+                    {inputs.map((input)=>(
+                <InputForm
+                        key = {input.id}
+                        {...input}
+                        onChange = {onChange}
+                        />
+        ))}
+                <button>Add</button>
+                </form>
         </div>
     );
-    }
-}
+    };
+    
 
 export default App;
 
